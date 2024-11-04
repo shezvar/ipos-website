fe619a7552069ef40148be9c2ec35f6f43d66a20
Segun Oroyo <shezvar@gmail.com>
2024-11-04 15:26:37 +0000

    color to ipos primary

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+    // ...
+    theme: {
+      extend: {
+        colors: {
+          'ipos': '#298DBE', // Replace with your desired iPOS blue hex code
+        },
+      },
+    },
+    // ...
+  };
